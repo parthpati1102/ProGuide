5437df2141a33542edf988d08a660f074c8db534
Parth Patidar <parth.patidar1102@gmsil.com>
2026-01-17 12:49:57 +0530

    Email Updated using SendGrid

--- sendEmail.js
+++ sendEmail.js
@@ -1,26 +1,53 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "apikey", // this is literal string "apikey"
-    pass: process.env.SENDGRID_API_KEY,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.sendgrid.net",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: "apikey", // this is literal string "apikey"
+//     pass: process.env.SENDGRID_API_KEY,
+//   },
+// });
+
+// async function sendEmail(to, subject, message) {
+//   try {
+//     await transporter.sendMail({
+//       from: process.env.EMAIL_USER,
+//       to,
+//       subject,
+//       html: message,
+//     });
+//     console.log("Email sent successfully");
+//   } catch (error) {
+//     console.error("Email error:", error);
+//   }
+// }
+
+// module.exports = sendEmail;
+
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(to, subject, message) {
   try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    await sgMail.send({
       to,
+      from: {
+        email: process.env.EMAIL_USER,
+        name: "ProGuide",
+      },
       subject,
       html: message,
     });
-    console.log("Email sent successfully");
+
+    console.log("Email sent successfully via SendGrid API");
   } catch (error) {
-    console.error("Email error:", error);
+    console.error(
+      "SendGrid API error:",
+      error.response?.body || error.message
+    );
   }
 }
 
